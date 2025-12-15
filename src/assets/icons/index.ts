f5f422fef313b666239407ecfaedeb458a66b817
Mohammed Dawood <mohammedddawoodd@gmail.com>
2025-12-15 10:48:12 +0300

    feat: icons script for automated icon components from svg files

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,32 @@
+// Auto-generated file - Do not edit manually
+// This file is updated automatically by the update-icons script
+// Last updated: 2025-12-15T07:28:05.736Z
+
+export { default as ArrowDownIcon } from "./svg/arrow-down.svg";
+export { default as ArrowLeftIcon } from "./svg/arrow-left.svg";
+export { default as ArrowRightIcon } from "./svg/arrow-right.svg";
+export { default as ArrowUpIcon } from "./svg/arrow-up.svg";
+export { default as BreadcrumbSeparatorIcon } from "./svg/breadcrumb-separator.svg";
+export { default as CalendarIcon } from "./svg/calendar.svg";
+export { default as CheckCircleIcon } from "./svg/check-circle.svg";
+export { default as ChevronDownIcon } from "./svg/chevron-down.svg";
+export { default as CloseIcon } from "./svg/close.svg";
+export { default as CommandIcon } from "./svg/command.svg";
+export { default as CopyIcon } from "./svg/copy.svg";
+export { default as DeleteIcon } from "./svg/delete.svg";
+export { default as EnterIcon } from "./svg/enter.svg";
+export { default as EscIcon } from "./svg/esc.svg";
+export { default as LinkIcon } from "./svg/link.svg";
+export { default as MaximizeIcon } from "./svg/maximize.svg";
+export { default as MessagesIcon } from "./svg/messages.svg";
+export { default as MinimizeIcon } from "./svg/minimize.svg";
+export { default as NotificationIcon } from "./svg/notification.svg";
+export { default as OutlinedCircleIcon } from "./svg/outlined-circle.svg";
+export { default as SearchIcon } from "./svg/search.svg";
+export { default as SettingsIcon } from "./svg/settings.svg";
+export { default as ShareIcon } from "./svg/share.svg";
+export { default as StarFilledIcon } from "./svg/star-filled.svg";
+export { default as StarOutlinedIcon } from "./svg/star-outlined.svg";
+export { default as UserIcon } from "./svg/user.svg";
+export { default as UsersIcon } from "./svg/users.svg";
+export { default as VerifiedIcon } from "./svg/verified.svg";
