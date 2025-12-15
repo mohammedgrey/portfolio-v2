2ba00ffde41d6298472fa81eedfbc1ef1ae08769
Mohammed Dawood <mohammedddawoodd@gmail.com>
2025-12-15 09:26:40 +0300

    feat: setting up eslint and typescript checks on file changes

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Enable type checking during development and build
+    ignoreBuildErrors: false,
+  },
 };
 
 export default withNextIntl(nextConfig);
